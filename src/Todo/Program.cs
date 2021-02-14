1c5be3a873f273c776a3c20acb066deb6dcb0a97
Jon Morgan <jmorjsm@gmail.com>
2021-02-14 17:13:03 +0000

    adding System.CommandLine, readme, initial scaffolding

--- src/Todo/Program.cs
+++ src/Todo/Program.cs
@@ -1,12 +1,50 @@
 ﻿using System;
+using System.CommandLine;
+using System.CommandLine.Invocation;
+using System.IO;
 
 namespace Todo
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var rootCommand = new RootCommand
+            {
+                new Command("init", "creates a new todo list")
+                {
+                    Handler = CommandHandler.Create<int>((intOption) =>
+                    {
+                        Console.WriteLine("Init command:");
+                    }),
+                },
+                new Command("new-day", "moves ongoing tasks to a new todo list for to day")
+                {
+                    Handler = CommandHandler.Create<int>((intOption) =>
+                    {
+                        Console.WriteLine("New-day command:");
+                    }),
+                },
+                new Command("view",	"views the todo list")
+                {
+                    Handler = CommandHandler.Create<int>((intOption) =>
+                    {
+                        Console.WriteLine("View command:");
+                    }),
+                },
+                new Command("review", "steps through viewing each task")
+                {
+                    Handler = CommandHandler.Create<int>((intOption) =>
+                    {
+                        Console.WriteLine("Review command:");
+                    }),
+                },
+            };
+
+            rootCommand.Description = "Todo";
+
+            // Parse the incoming args and invoke the handler
+            return rootCommand.InvokeAsync(args).Result;
         }
     }
 }
